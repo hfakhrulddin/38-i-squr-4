f59a946b2a4c53fdab2b00469b55039450b7b1a0
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-07 21:24:28 -0700

    init

---
+++ Isqur4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Isqur4
+{
+    public static class Program
+    {
+        static IEnumerable Squares(this int from, int to)
+        {
+            for (int i = from; i <= to; i++)
+            {
+                yield return (int)i * i;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var min = 1;
+            foreach (int i in min.Squares(4))
+            {
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+        }
+    }
+}
